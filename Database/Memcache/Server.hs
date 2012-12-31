87a9a9678544f4044a26f8c36c0ee9f0e44fb7da
Montez Fitzpatrick <montezf@gmail.com>
2012-12-31 13:01:52 -0600

    Small documentation update

--- Database/Memcache/Server.hs
+++ Database/Memcache/Server.hs
@@ -1,4 +1,10 @@
 -- | Module for Memcache Server Nodes
+--
+--   This is a wrapper for Server Nodes which will be added
+--   to the cluster to be managed.  Use /defaultServer/ for 
+--   sensible defaults when creating or use /setServer/ for
+--   some slightly more fined grained control on how the nodes
+--   are represented.
 module Database.Memcache.Server
   (
    -- * Server Type
@@ -6,7 +12,7 @@ module Database.Memcache.Server
    -- * Supporting Types
   , Transport(..)
   , Authentication(..)
-  , Pooling(..)
+  , Pooling
   , Weight(..)
    -- * Server Creation
   , defaultServer
