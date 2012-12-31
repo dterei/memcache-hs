023907df1d943eacec3584c703cb543f6d00ec75
Montez Fitzpatrick <montezf@gmail.com>
2012-12-31 12:15:54 -0600

    @IP moving connection functions which were originally defined here into Database.Memcache.Connection.Internal, this module now basically just re-exports the child Server.Internal.hs module

--- Database/Memcache/Server.hs
+++ Database/Memcache/Server.hs
@@ -1,64 +1,16 @@
--- | Handles the connections between a memcache client and the various servers
--- that make up the cluster.
-module Database.Memcache.Server (
-        Connection(..), newMemcacheClient, send, sendRecv, recv
-    ) where
+-- | Module for Memcache Server Nodes
+module Database.Memcache.Server
+  (
+   -- * Server Type
+    Server
+   -- * Supporting Types
+  , Transport(..)
+  , Authentication(..)
+  , Pooling(..)
+  , Weight(..)
+   -- * Server Creation
+  , defaultServer
+  , setServer
+  ) where
 
-import Database.Memcache.Types
-import Database.Memcache.Wire
-
-import Control.Exception
-import qualified Data.ByteString.Lazy as L
-import Data.ByteString.Builder
-
-import Network.BSD (getProtocolNumber, getHostByName, hostAddress)
-import Network.Socket hiding (send, recv)
-import qualified Network.Socket.ByteString as N
-
--- | A Memcache connection handle.
--- XXX: Should make abstract
-data Connection = Conn {
-        conn :: Socket
-    }
-
--- | Establish a new connection to a memcache backend.
-newMemcacheClient :: HostName -> PortNumber -> IO Connection
-newMemcacheClient h p = do
-    s <- connectTo h p
-    setSocketOption s KeepAlive 1
-    setSocketOption s NoDelay 1
-    return (Conn s)
-
--- | Connect to a host. (Internal, socket version of connectTo).
-connectTo :: HostName -> PortNumber -> IO Socket
-connectTo host port = do
-    proto <- getProtocolNumber "tcp"
-    bracketOnError
-        (socket AF_INET Stream proto)
-        (close)
-        (\sock -> do
-            h <- getHostByName host
-            connect sock (SockAddrInet port (hostAddress h))
-            return sock
-        )
-
--- | Send a request to the memcache cluster.
-send :: Connection -> Request -> IO ()
-send c m = N.sendAll (conn c) (L.toStrict $ toLazyByteString $ szRequest m)
-
--- | Send a receieve a single request/response pair to the memcache cluster.
-sendRecv :: Connection -> Request -> IO Response
-sendRecv c m = do
-    send c m
-    recv c
-
--- | Retrieve a single response from the memcache cluster.
-recv :: Connection -> IO Response
-recv c = do
-    -- XXX: recv may return less.
-    header <- N.recv (conn c) mEMCACHE_HEADER_SIZE
-    let h = dzHeader' (L.fromStrict header)
-    body <- N.recv (conn c) (fromIntegral $ bodyLen h)
-    let b = dzBody' h (L.fromStrict body)
-    return b
-
+import Database.Memcache.Server.Internal
